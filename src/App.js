637779a49fe53619208c76c9208e9c7966ebfca1
David Vitas <david.vitas@hotmail.com>
2024-05-23 20:19:54 +0200

    adding basic product detail page view and button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Menu from "./pages/Menu";
 import Profile from "./pages/Profile";
 import Cart from "./pages/Cart";
 import "./App.css";
+import ProductDetails from "./pages/ProductDetails";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/ourmenu" element={<Menu />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
       </Routes>
     </Router>
   );
